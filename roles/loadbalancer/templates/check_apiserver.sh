c4c2ef32c7b1d383744bee55118014c5158d7a1c
ManhDat <datvm@vccloud.vn>
2024-07-03 11:03:39 +0700

    update: Fixing...

--- roles/loadbalancer/templates/check_apiserver.sh
+++ roles/loadbalancer/templates/check_apiserver.sh
@@ -1,4 +1,3 @@
-
  #!/bin/sh
 
  errorExit() {
@@ -6,8 +5,14 @@
      exit 1
  }
 
+{% if lb.haproxy_vip %}
  curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
  if ip addr | grep -q {{ virtual_ip }}; then
      curl --silent --max-time 2 --insecure https://{{ virtual_ip }}:6443/ -o /dev/null || errorExit "Error GET https://{{ virtual_ip }}:6443/"
  fi
-
+{% else %}
+ curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
+ if ip addr | grep -q {{ lb.virtual_ip }}; then
+     curl --silent --max-time 2 --insecure https://{{ lb.virtual_ip }}:6443/ -o /dev/null || errorExit "Error GET https://{{ lb.virtual_ip }}:6443/"
+ fi
+{% endif %}
