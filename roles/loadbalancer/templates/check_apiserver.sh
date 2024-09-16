7d39c0044855eec0d6befc449c642b401656bbbd
nulldoot2k <companydatv412@gmail.com>
2024-09-16 22:38:52 +0700

    UP: update scripts cluster kubernetes with ansible.

--- roles/loadbalancer/templates/check_apiserver.sh
+++ roles/loadbalancer/templates/check_apiserver.sh
@@ -5,7 +5,7 @@
      exit 1
  }
 
-{% if lb.haproxy_vip %}
+{% if lb.active_ha_vip %}
  curl --silent --max-time 2 --insecure https://localhost:6443/ -o /dev/null || errorExit "Error GET https://localhost:6443/"
  if ip addr | grep -q {{ virtual_ip }}; then
      curl --silent --max-time 2 --insecure https://{{ virtual_ip }}:6443/ -o /dev/null || errorExit "Error GET https://{{ virtual_ip }}:6443/"
